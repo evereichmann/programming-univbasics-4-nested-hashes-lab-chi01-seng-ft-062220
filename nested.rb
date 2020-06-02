ab9df9d89fe5afd61502b6a4ef67261de07ab7c1
Eve Reichmann <ereichmann4@aol.com>
2020-06-02 14:26:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,8 +20,6 @@ end
 
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -56,7 +54,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -82,7 +80,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:add_character][:Matz]
 end
 
 def changing_alan
